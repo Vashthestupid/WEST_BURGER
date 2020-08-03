3c7f5f56f3a7fbbbe04019088b921ffcf1a42b59
Loïc Tauveron <loic.tauveron62@gmail.com>
2020-08-03 17:25:01 +0200

    first commit

---
+++ WEST_BURGER_HTML_CSS/public/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#link_sous_menu').click(function(){
+        $('#sous_menu').show();
+    })
+    $('#sous_menu').mouseleave(function(){
+        $('#sous_menu').hide();
+    })
+
+});
