c29e5ff9040da0c1b3d100e58932024d08ad24fd
Loïc Tauveron <loic.tauveron62@gmail.com>
2020-08-04 16:19:26 +0200

    Reprise à zero de la page d'accueil , création de la page inscription et connexion

--- WEST_BURGER_HTML_CSS/public/js/main.js
+++ WEST_BURGER_HTML_CSS/public/js/main.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
-    $('#link_sous_menu').click(function(){
-        $('#sous_menu').show();
-    })
-    $('#sous_menu').mouseleave(function(){
-        $('#sous_menu').hide();
+
+    // Faire apparaître le sous menu
+    $('#lien_sous_menu').click(function(){
+        $('#sous_menu').toggle();
     })
 
-});
+    // Faire apparaître un formulaire au clic 
+    $('#btnInscription').click(function(){
+        $('#inscription').show();
+        $('#connexion').hide();
+        // Changer la couleur des boutons en fonction de
+        // celui qui est actif
+        $('#btnInscription').removeClass('btn-secondary');
+        $('#btnInscription').addClass('btn-primary');
+        $('#btnConnexion').removeClass('btn-primary');
+        $('#btnConnexion').addClass('btn-secondary');
+    })
+
+    $('#btnConnexion').click(function(){
+        $('#connexion').show();
+        $('#inscription').hide();
+        // Changer la couleur des boutons en fonction de
+        // celui qui est actif
+        $('#btnConnexion').removeClass('btn-secondary');
+        $('#btnConnexion').addClass('btn-primary');
+        $('#btnInscription').removeClass('btn-primary');
+        $('#btnInscription').addClass('btn-secondary');
+    })
+})
